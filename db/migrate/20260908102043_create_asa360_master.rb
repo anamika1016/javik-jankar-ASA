9eafb04b1da81d24561c7cec5be4c151e4fae7e2
Roshni Tiwari <roshnitiwaripip@gmail.com>
2026-09-19 16:16:05 +0530

    fixes for quize

--- db/migrate/20260908102043_create_asa360_master.rb
+++ db/migrate/20260908102043_create_asa360_master.rb
@@ -1,6 +1,6 @@
 class CreateAsa360Master < ActiveRecord::Migration[8.1]
   def change
-    create_table :asa360_master do |t|
+    create_table :asa360_master, if_not_exists: true do |t|
       t.string :o_type, limit: 50
       t.string :o_name, limit: 200
       t.integer :o_id
